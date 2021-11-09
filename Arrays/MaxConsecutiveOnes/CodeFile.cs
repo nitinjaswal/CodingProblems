85047e9281a9b04350020bbbecc9dfa50df37bcc
Nitin <nitinjas.chd@gmal.com>
2021-11-09 22:57:35 +0530

    Add project files.

---
+++ Arrays/MaxConsecutiveOnes/CodeFile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+
+        int counter = 0;
+        ArrayList window = new ArrayList();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                counter++;
+                if (i == nums.Length - 1)
+                {
+                    window.Add(counter);
+                }
+            }
+            else
+            {
+                window.Add(counter);
+                counter = 0;
+            }
+        }
+
+        int max = 0;
+
+        foreach (int i in window)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
