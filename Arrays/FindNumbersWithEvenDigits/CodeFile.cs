d9b64532a8f93dbcc0c99d451f795f36c0006d50
Nitin <nitinjas.chd@gmal.com>
2021-11-10 22:28:59 +0530

    Solution Find numbers with even digits in array

--- Arrays/FindNumbersWithEvenDigits/CodeFile.cs
+++ Arrays/FindNumbersWithEvenDigits/CodeFile.cs
@@ -1,1 +1,31 @@
-﻿
+﻿public class SolutionFindNumbersWithEvenDigits
+{
+    public int FindNumbers(int[] nums)
+    {
+        int counter = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (checkEven(nums[i].ToString().Length))
+            {
+
+                counter++;
+
+            }
+
+        }
+        return counter;
+    }
+
+    private static bool checkEven(int length)
+    {
+        if (length % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
