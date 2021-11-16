0ee5a174fa3a8a4adb44c0db265fda944ec00a1c
Nitin <nitinjas.chd@gmal.com>
2021-11-16 21:41:41 +0530

    readmeAdded solution for DuplicateZeros

--- Program.cs
+++ Program.cs
@@ -6,36 +6,8 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] nums = { 12, 345, 2, 6, 7896 };
-            int counter = 0;
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (checkEven(nums[i].ToString().Length))
-                {
-
-                    counter++;
-
-                }
-
-            }
-
-
-            Console.WriteLine(counter);
-            Console.ReadKey();
-        }
-
-        private static bool checkEven(int length)
-        {
-            if (length % 2 == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
 
         }
+
     }
 }
