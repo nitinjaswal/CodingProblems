72698bc8b666f51b205cc4d46d4a5e16abec460f
nitin <nitinjas.chd@gmail.com>
2021-12-06 22:46:55 +0530

    Solution : If mountain array

---
+++ Arrays/IfMountainArray/CodeFile.cs
@@ -0,0 +1,49 @@
+﻿public class SolutionIsMountainArray
+{
+    public bool ValidMountainArray(int[] arr)
+    {
+        bool isMountain = false;
+        if (arr.Length >= 3)
+        {
+            int i = 0;
+            while (i < arr.Length - 1)
+            {
+                if (arr[i + 1] > arr[i])
+                {
+                    isMountain = true;
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            
+            if (i == arr.Length - 1)
+            {
+                isMountain = false;
+            }
+
+            if (isMountain)
+            {
+                for (int j = i; j < arr.Length - 1; j++)
+                {
+                    if (arr[j + 1] < arr[j])
+                    {
+                        isMountain = true;
+                    }
+                    else
+                    {
+                        isMountain = false;
+                        break;
+                    }
+
+                }
+            }
+
+
+        }
+        return isMountain;
+    }
+}
