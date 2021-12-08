800ee24577bf37122b05f92d5007ccab779b263d
nitin <nitinjas.chd@gmail.com>
2021-12-08 13:19:55 +0530

    Solution: MoveZeroes

---
+++ Arrays/MoveZeroes/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿public class SolutionMoveZeroes
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int i = 0;
+        for (int j = 0; j <= nums.Length - 1;)
+        {
+            if (nums[j] != 0)
+            {
+                nums[i] = nums[j];
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        while (i <= nums.Length - 1)
+        {
+            nums[i++] = 0;
+        }
+    }
+}
