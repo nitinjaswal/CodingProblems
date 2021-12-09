ee2522d5ff2f19085e1535b63a9d55f66d3ab32a
nitin <nitinjas.chd@gmail.com>
2021-12-09 13:14:09 +0530

    Solution:Move array by parity

---
+++ Arrays/MoveArrayByParity/CodeFile.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums;
+        }
+
+        int i = 0;//
+        int temp = 0;
+        for (int j = 0; j <= nums.Length - 1;)
+        {
+            if (nums[j] % 2 != 0)
+            {
+                j++;
+            }
+            else
+            {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            }
+        }
+        return nums;
+    }
+}
