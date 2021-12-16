6f8af300240253f3263ff9a23694ee1c1b46e963
nitin <nitinjas.chd@gmail.com>
2021-12-16 19:27:19 +0530

    Solution: Third max element in array

---
+++ Arrays/ThirdMaxNumber/CodeFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+public class SolutionThirdMax
+{
+    public int ThirdMax(int[] arr)
+    {
+        int maxCounter = 0;
+        Array.Sort(arr);
+        int max = 0;
+        Hashtable hashtable = new Hashtable();
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            if (!hashtable.ContainsValue(arr[i]))
+            {
+                hashtable.Add(arr[i], arr[i]);
+                maxCounter++;
+                if (maxCounter == 3)
+                {
+                    max = arr[i];
+                    break;
+                }
+            }
+        }
+
+        return maxCounter != 3 ? arr[arr.Length - 1] : max;
+    }
+}
