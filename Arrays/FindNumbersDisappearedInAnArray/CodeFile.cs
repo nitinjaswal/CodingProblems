734a63b4e2c43ff28ba3930cf64bb4d974f3bcca
nitin <nitinjas.chd@gmail.com>
2021-12-16 19:29:48 +0530

    Solution: Find All Numbers Disappeared in an Array

---
+++ Arrays/FindNumbersDisappearedInAnArray/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SolutionFindDisappearedNumbers
+{
+    public IList<int> FindDisappearedNumbers(int[] arr)
+    {
+        List<int> missingElements = new List<int>();
+        int j = 0;
+        for (int i = 0; i <= arr.Length - 1; i++)
+        {
+            j = Math.Abs(arr[i]) - 1;
+            arr[j] = Math.Abs(arr[j]) * -1;
+        }
+        for (j = 0; j <= arr.Length - 1; j++)
+        {
+            if (arr[j] > 0)
+            {
+                missingElements.Add(j + 1);
+            }
+        }
+        return missingElements;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,31 +11,6 @@ namespace Challenges
         {
             int[] arr = { 1,1 };
             //{ 1, 2, 2 };
-            //var thirdMaxNumber = FindDisappearedNumbers(arr);
-
-        }
-
-        public static IList<int> FindDisappearedNumbers(int[] arr)
-        {
-            //int counter = 0;
-
-
-            //List<int> missingElements = new List<int>();
-            //int j = 0;
-            //for (int i = 0; i <= arr.Length - 1; i++)
-            //{
-            //    j = Math.Abs(arr[i]) - 1; 
-            //    arr[j] = Math.Abs(arr[j]) * -1;
-            //}
-            //for (j = 0; j <= arr.Length - 1; j++)
-            //{
-            //    if (arr[j] > 0)
-            //    {
-            //        missingElements.Add(j + 1);
-            //    }
-            //}
-            //return missingElements;
-
         }
     }
 }
