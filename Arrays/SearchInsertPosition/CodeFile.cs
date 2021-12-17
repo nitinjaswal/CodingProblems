2a2c92cc8ab587bb176e40d5fbe8eb02ce02f40d
nitin <nitinjas.chd@gmail.com>
2021-12-17 15:08:03 +0530

    Solution: Search Insert Position with O(log n ) complexity

---
+++ Arrays/SearchInsertPosition/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿public class SolutionSearchInsert
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int index = 0;
+        int low = 0;
+        int high = nums.Length - 1;
+
+        //Using binary search to find index in O(Log n) time
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+
+            if (target > nums[mid])
+            {
+                low = mid + 1;
+                index = low;
+            }
+            else
+            {
+                index = mid;
+                high = mid - 1;
+            }
+        }
+        return index;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,8 +9,41 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1,1 };
+            int[] arr = { 1 };
+            int target = 0;
+            int index = SearchInsert(arr, target);
             //{ 1, 2, 2 };
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int index = 0;
+            int low = 0;
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+
+                if (target > nums[mid])
+                {
+                    low = mid + 1;
+                    index = low;
+                }
+                else
+                {
+                    index = mid;
+                    high = mid - 1;                
+
+                }
+            }
+            return index;
+
         }
     }
 }
