35954ba0993895ccda197487afae4023da641007
nitin <nitinjas.chd@gmail.com>
2021-12-23 23:37:34 +0530

    Solution: Detect loop in linked list using HashTable

---
+++ LinkedList/DetectLoopLinkedListUsingHashSet/CodeFile.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Collections.Generic;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+//Brute force approach using hashtable. But space complexity is O(n): due to extra space
+public class SolutionHasCycle
+{
+    public bool HasCycle(ListNode head)
+    {
+        HashSet<ListNode> hashNode = new HashSet<ListNode>();
+        while (head != null)
+        {
+            if (hashNode.Contains(head))
+            {
+                return true;
+            }
+            else
+            {
+                hashNode.Add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -10,18 +10,33 @@ namespace Challenges
         static void Main(string[] args)
         {
             MyLinkedList obj = new MyLinkedList();
-            obj.AddAtHead(1);
-            obj.AddAtTail(3);
-            obj.AddAtIndex(1, 2);
-            obj.Get(1);
-            obj.DeleteAtIndex(1);
-            obj.Get(1);
-            obj.Get(3);
-            obj.DeleteAtIndex(3);
-            obj.DeleteAtIndex(0);
-            obj.Get(0);
-            obj.DeleteAtIndex(0);
-            obj.Get(0);
+            obj.AddAtHead(20);
+            obj.AddAtHead(4);
+            obj.AddAtHead(15);
+            obj.AddAtHead(10);
+
+            /*Create loop for testing */
+            obj.head.nextNode.nextNode.nextNode.nextNode = obj.head;
+
+            bool exist = HasCycle(obj.head);
+        }
+
+        public static bool HasCycle(Node head)
+        {
+            HashSet<Node> hashNode = new HashSet<Node>();
+            while (head!= null)
+            {
+                if (hashNode.Contains(head))
+                {
+                    return true;
+                }
+                else
+                {
+                    hashNode.Add(head);
+                }
+                head = head.nextNode;
+            }
+            return false;
         }
     }
 
@@ -45,27 +60,6 @@ namespace Challenges
         {
 
         }
-        public int Get(int index)
-        {
-
-            if (index >= length || index < 0)
-            {
-                return -1;
-            }
-            else
-            {
-                var currentode = this.head;
-                int counter = 0;
-                while (counter != index)
-                {
-                    currentode = currentode.nextNode;
-                    counter++;
-                }
-                return currentode.value;
-            }
-
-        }
-
         public void AddAtHead(int val)
         {
             Node newNode = new Node(val);
@@ -73,88 +67,9 @@ namespace Challenges
             this.head = newNode;
             length++;
         }
-
-        public void AddAtTail(int val)
-        {
-            if (length == 0)
-            {
-                AddAtHead(val);
-                return;
-            }
-            else
-            {
-                Node newNode = new Node(val);
-                var currentode = this.head;
-                while (currentode.nextNode != null)
-                {
-                    currentode = currentode.nextNode;
-                }
-                currentode.nextNode = newNode;
-                newNode.nextNode = null;
-            }
-            length++;
-        }
-
-        public void AddAtIndex(int index, int val)
-        {
-            if (index > length)
-            {
-                return;
-            }
-            //Insert before first node or Head node
-            if (index == 0)
-            {
-                AddAtHead(val);
-                return;
-            }
-  
-            if (index == length)
-            {
-                AddAtTail(val);
-                return;
-            }
-            else //Insert at given position
-            {
-                Node newNode = new Node(val);
-                var currentode = this.head;
-                //Here we are using for loop because we know at which position we have to stop to insert new node.
-                for (int i = 0; i < index - 1; i++)
-                {
-                    currentode = currentode.nextNode;
-                }
-                var temp = currentode.nextNode;
-                currentode.nextNode = newNode;
-                newNode.nextNode = temp;
-                length++;
-            }
-        }
-
-        public void DeleteAtIndex(int index)
-        {
-            var currentNode = this.head;
-            var previousNode = this.head;
-            if (index >= length || index < 0)
-            {
-                return;
-            }
-            //Delete first node or head node
-            if (index == 0)
-            {
-                this.head = currentNode.nextNode;
-            }
-            else
-            {
-                //Delete a node from any given index
-                for (int i = 0; i < index; i++)
-                {
-                    previousNode = currentNode;
-                    currentNode = currentNode.nextNode;
-                }
-                previousNode.nextNode = currentNode.nextNode;                
-            }
-            length--;
-        }
     }
+
+
 }
 
 
