690b9ff0a112ba960aaa549aa0706aed12382920
nitin <nitinjas.chd@gmail.com>
2021-12-24 23:36:47 +0530

    Solution: Detect loop in linked list in O(1) space

---
+++ LinkedList/DetectLoopUsingFloydAlgoOrTwoPointers/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿public class SolutionHasCycle
+{
+    public bool HasCycle(ListNode head)
+    {
+        //Solvin the problem with 2 pointer approach using slow and fast pointer
+        var slowPointer = head;
+        var fastPointer = head;
+
+        //If first node is null or there is only head node, it means there is no cycle
+        while (fastPointer != null && fastPointer.next != null)
+        {
+            slowPointer = slowPointer.next;//slow pointer will move 1 step
+            fastPointer = fastPointer.next.next;//fast pointer will move 2 step ahead
+
+            if (slowPointer == fastPointer)///At some point "fastpointer" will cross "slowpointer"
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,76 +1,78 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace Challenges
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            MyLinkedList obj = new MyLinkedList();
-            obj.AddAtHead(20);
-            obj.AddAtHead(4);
-            obj.AddAtHead(15);
-            obj.AddAtHead(10);
+//namespace Challenges
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyLinkedList obj = new MyLinkedList();
+//            obj.AddAtHead(1);
+//            obj.AddAtHead(4);
+//            //obj.AddAtHead(15);
+//            //obj.AddAtHead(10);
 
-            /*Create loop for testing */
-            obj.head.nextNode.nextNode.nextNode.nextNode = obj.head;
+//            /*Create loop for testing */
+//            //obj.head.nextNode.nextNode.nextNode.nextNode = obj.head;
 
-            bool exist = HasCycle(obj.head);
-        }
+//            bool exist = HasCycle(obj.head);
+//        }
 
-        public static bool HasCycle(Node head)
-        {
-            HashSet<Node> hashNode = new HashSet<Node>();
-            while (head!= null)
-            {
-                if (hashNode.Contains(head))
-                {
-                    return true;
-                }
-                else
-                {
-                    hashNode.Add(head);
-                }
-                head = head.nextNode;
-            }
-            return false;
-        }
-    }
+//        public bool HasCycle(ListNode head)
+//        {
+//            //Solvin the problem with 2 pointer approach using slow and fast pointer
+//            var slowPointer = head;
+//            var fastPointer = head;
 
-    public class Node
-    {
-        public int value;
-        public Node nextNode;
+//            //If first node is null or there is only head node, it means there is no cyc
+//            while (fastPointer != null && fastPointer.next != null)
+//            {
+//                slowPointer = slowPointer.next;//slow pointer will move 1 step
+//                fastPointer = fastPointer.next.next;//fast pointer will move 2 step ahead
 
-        public Node(int value)
-        {
-            this.value = value;
-            this.nextNode = null;
-        }
-    }
-    public class MyLinkedList
-    {
-        public Node head;
-        public Node tail;
-        public int length;
-        public MyLinkedList()
-        {
+//                if (slowPointer == fastPointer)///At some point "fastpointer" will cross "slowpointer"
+//                {
+//                    return true;
+//                }
+//            }
+//            return false;
+//        }
+//    }
 
-        }
-        public void AddAtHead(int val)
-        {
-            Node newNode = new Node(val);
-            newNode.nextNode = this.head;
-            this.head = newNode;
-            length++;
-        }
-    }
+//    public class Node
+//    {
+//        public int value;
+//        public Node nextNode;
+
+//        public Node(int value)
+//        {
+//            this.value = value;
+//            this.nextNode = null;
+//        }
+//    }
+//    public class MyLinkedList
+//    {
+//        public Node head;
+//        public Node tail;
+//        public int length;
+//        public MyLinkedList()
+//        {
+
+//        }
+//        public void AddAtHead(int val)
+//        {
+//            Node newNode = new Node(val);
+//            newNode.nextNode = this.head;
+//            this.head = newNode;
+//            length++;
+//        }
+//    }
 
 
-}
+//}
 
 
 
