ee10dee0a70fd84ea89317d7cd7d17fe1576b8a2
nitin <nitinjas.chd@gmail.com>
2022-01-03 13:17:27 +0530

    Solution: Find intersection of 2 linked list using O(1) space and O(m+n) time complexity

---
+++ LinkedList/DetectIntersectionLinkedList/CodeFile.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+class LinkedList
+{
+
+    Node head1, head2;
+
+    public class Node
+    {
+
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        // creating first linked list
+        list.head1 = new Node(3);
+        list.head1.next = new Node(6);
+        list.head1.next.next = new Node(9);
+        list.head1.next.next.next = new Node(15);
+        list.head1.next.next.next.next = new Node(30);
+
+        // creating second linked list
+        list.head2 = new Node(10);
+        list.head2.next = new Node(15);
+        list.head2.next.next = new Node(30);
+
+        var node = list.GetIntersectionNode(list, list);
+    }
+
+    public Node GetIntersectionNode(LinkedList list1, LinkedList list2)
+    {
+        var head1 = list1.head1;
+        var head2 = list2.head2;
+        int count1 = getCount(head1);//Get count of linked list 1
+        int count2 = getCount(head2);//Get count of linked list 2
+
+        //Calculating diff of noded between both linked list to check how many extra nodes are present in larger list
+        int diff = Math.Abs(count1 - count2);
+
+        if (count1 > count2)
+        {
+            //if list1 count is greater than the list2, then we move pointer1 position to the (diff between 2 nodes)
+            //else we move pointer2
+            for (int i = 0; i < diff; i++)
+            {
+                head1 = head1.next;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < diff; i++)
+            {
+                head2 = head2.next;
+            }
+        }
+
+        //Now both pointers are at same distance from intersection nodes(if ther are any)
+        while (head1 != head2)
+        {
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+
+        return head1;
+    }
+
+
+
+    public int getCount(Node node)
+    {
+        Node current = node;
+        int count = 0;
+
+        while (current.next != null)
+        {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,88 +5,96 @@ using System.Linq;
 
 namespace Challenges
 {
-    class Program
+    class LinkedList
     {
-        static void Main(string[] args)
+
+        Node head1, head2;
+
+        public class Node
         {
-            MyLinkedList obj = new MyLinkedList();
-            obj.AddAtHead(4);
-            //obj.AddAtHead(0);
-            //obj.AddAtHead(2);
-            //obj.AddAtHead(3);
 
-            /*Create loop for testing */
-            obj.head.nextNode = null;
+            public int data;
+            public Node next;
 
-            var nodr = HasCycle(obj.head);
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
         }
 
-        public static Node HasCycle(Node head)
+        public static void Main(String[] args)
         {
-            //Solvin the problem with 2 pointer approach using slow and fast pointer
-            var slowPointer = head;
-            var fastPointer = head;
-            //If first node is null or there is only head node, it means there is no cyc
-            while (fastPointer != null && fastPointer.nextNode != null)
-            {
-                slowPointer = slowPointer.nextNode;//slow pointer will move 1 step
-                fastPointer = fastPointer.nextNode.nextNode;//fast pointer will move 2 step ahead
+            LinkedList list = new LinkedList();
 
-                if (slowPointer == fastPointer)///At some point "fastpointer" will cross "slowpointer"
+            // creating first linked list
+            list.head1 = new Node(3);
+            list.head1.next = new Node(6);
+            list.head1.next.next = new Node(9);
+            list.head1.next.next.next = new Node(15);
+            list.head1.next.next.next.next = new Node(30);
+
+            // creating second linked list
+            list.head2 = new Node(10);
+            list.head2.next = new Node(15);
+            list.head2.next.next = new Node(30);
+
+            var node = list.GetIntersectionNode(list, list);
+        }
+
+        public Node GetIntersectionNode(LinkedList list1, LinkedList list2)
+        {
+            var head1 = list1.head1;
+            var head2 = list2.head2;
+            int count1 = getCount(head1);//Get count of linked list 1
+            int count2 = getCount(head2);//Get count of linked list 2
+
+            //Calculating diff of noded between both linked list to check how many extra nodes are present in larger list
+            int diff = Math.Abs(count1 - count2);
+
+            if (count1 > count2)
+            {
+                //if list1 count is greater than the list2, then we move pointer1 position to the (diff between 2 nodes)
+                //else we move pointer2
+                for (int i = 0; i < diff; i++)
                 {
-                    break;
+                    head1 = head1.next;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < diff; i++)
+                {
+                    head2 = head2.next;
                 }
             }
 
-            if (slowPointer != fastPointer)
+            //Now both pointers are at same distance from intersection nodes(if ther are any)
+            while (head1 != head2)
             {
-                return null;
+                head1 = head1.next;
+                head2 = head2.next;
             }
 
-            if (head.nextNode == null)
+            return head1;
+        }
+
+
+
+        public int getCount(Node node)
+        {
+            Node current = node;
+            int count = 0;
+
+            while (current.next != null)
             {
-                return null;
+                count++;
+                current = current.next;
             }
-            slowPointer = head;
-            while (slowPointer != fastPointer)
-            {
-                slowPointer = slowPointer.nextNode;//slow pointer will move 1 step
-                fastPointer = fastPointer.nextNode;//fast pointer will move 1 step ahead
-            }
-            
-            return slowPointer;
+
+            return count;
         }
     }
-
-    public class Node
-    {
-        public int value;
-        public Node nextNode;
-
-        public Node(int value)
-        {
-            this.value = value;
-            this.nextNode = null;
-        }
-    }
-    public class MyLinkedList
-    {
-        public Node head;
-        public Node tail;
-        public int length;
-        public MyLinkedList()
-        {
-
-        }
-        public void AddAtHead(int val)
-        {
-            Node newNode = new Node(val);
-            newNode.nextNode = this.head;
-            this.head = newNode;
-            length++;
-        }
-    }
-
 
 }
 
