722c521f646e908c16fe7fbc8614b9a8fac1f577
nitin <nitinjas.chd@gmail.com>
2022-01-05 23:19:10 +0530

    Brute force Solution: Delete nth node from end of the linked list

---
+++ LinkedList/RemoveNthNodeFromEndBruteForce/CodeFile.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class SolutionRemoveNthFromEnd
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        if (head.next == null)
+        {
+            return null;
+        }
+        int count = getCount(head);
+        int counter = 1;
+        var currentNode = head;
+        if (count == n)
+        {
+            return head.next;
+        }
+        //Loop till the length of linked list minus node to delete
+        while (counter < count - n)
+        {
+            counter++;
+            currentNode = currentNode.next;
+        }
+
+        var temp = currentNode.next.next;
+        currentNode.next = temp;
+
+        return head;
+
+    }
+    public int getCount(ListNode node)
+    {
+        ListNode current = node;
+        int count = 0;
+
+        while (current != null)
+        {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -28,65 +28,51 @@ namespace Challenges
             LinkedList list = new LinkedList();
 
             // creating first linked list
-            list.head1 = new Node(3);
-            list.head1.next = new Node(6);
-            list.head1.next.next = new Node(9);
-            list.head1.next.next.next = new Node(15);
-            list.head1.next.next.next.next = new Node(30);
+            list.head1 = new Node(1);
+            list.head1.next = new Node(2);
+            list.head1.next.next = new Node(3);
+            list.head1.next.next.next = new Node(4);
+            list.head1.next.next.next.next = new Node(5);
 
             // creating second linked list
             list.head2 = new Node(10);
             list.head2.next = new Node(15);
             list.head2.next.next = new Node(30);
 
-            var node = list.GetIntersectionNode(list, list);
+            var node = list.RemoveNthFromEnd(list.head1, 2);
         }
 
-        public Node GetIntersectionNode(LinkedList list1, LinkedList list2)
+        public Node RemoveNthFromEnd(Node head, int n)
         {
-            var head1 = list1.head1;
-            var head2 = list2.head2;
-            int count1 = getCount(head1);//Get count of linked list 1
-            int count2 = getCount(head2);//Get count of linked list 2
-
-            //Calculating diff of noded between both linked list to check how many extra nodes are present in larger list
-            int diff = Math.Abs(count1 - count2);
-
-            if (count1 > count2)
+            if (head.next == null)
             {
-                //if list1 count is greater than the list2, then we move pointer1 position to the (diff between 2 nodes)
-                //else we move pointer2
-                for (int i = 0; i < diff; i++)
-                {
-                    head1 = head1.next;
-                }
+                return null;
             }
-            else
+            int count = getCount(head);
+            int counter = 1;
+            var currentNode = head;
+            if (count == n)
             {
-                for (int i = 0; i < diff; i++)
-                {
-                    head2 = head2.next;
-                }
+                return head.next;
+            }
+            while (counter < count - n)
+            {
+                counter++;
+                currentNode = currentNode.next;
             }
 
-            //Now both pointers are at same distance from intersection nodes(if ther are any)
-            while (head1 != head2)
-            {
-                head1 = head1.next;
-                head2 = head2.next;
-            }
+            var temp = currentNode.next.next;
+            currentNode.next = temp;
 
-            return head1;
+            return head;
         }
-
-
 
         public int getCount(Node node)
         {
             Node current = node;
             int count = 0;
 
-            while (current.next != null)
+            while (current != null)
             {
                 count++;
                 current = current.next;
