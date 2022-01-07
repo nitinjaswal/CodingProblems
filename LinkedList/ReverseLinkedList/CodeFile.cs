68df7f122207b55f1ac780880d7a81a025590d5a
nitin <nitinjas.chd@gmail.com>
2022-01-08 00:04:04 +0530

    Solution:Reverse a linked list

---
+++ LinkedList/ReverseLinkedList/CodeFile.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class SolutionReverseList
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode previousNode = null;
+        var currentNode = head;
+        while (currentNode != null)
+        {
+            ListNode temp = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = temp;
+        }
+        return previousNode;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Challenges
     class LinkedList
     {
 
-        Node head1, head2;
+        Node head, head2;
 
         public class Node
         {
@@ -28,57 +28,28 @@ namespace Challenges
             LinkedList list = new LinkedList();
 
             // creating first linked list
-            list.head1 = new Node(1);
-            list.head1.next = new Node(2);
-            list.head1.next.next = new Node(3);
-            list.head1.next.next.next = new Node(4);
-            list.head1.next.next.next.next = new Node(5);
+            list.head = new Node(1);
+            list.head.next = new Node(2);
+            list.head.next.next = new Node(3);
+            list.head.next.next.next = new Node(4);
+            list.head.next.next.next.next = new Node(5);
 
-            // creating second linked list
-            list.head2 = new Node(10);
-            list.head2.next = new Node(15);
-            list.head2.next.next = new Node(30);
-
-            var node = list.RemoveNthFromEnd(list.head1, 2);
+            var node = list.ReverseList(list.head);
         }
 
-        public Node RemoveNthFromEnd(Node head, int n)
+        public Node ReverseList(Node head)
         {
-            if (head.next == null)
-            {
-                return null;
-            }
-            int count = getCount(head);
-            int counter = 1;
+
+            Node previousNode = null;
             var currentNode = head;
-            if (count == n)
+            while (currentNode != null)
             {
-                return head.next;
+                Node temp = currentNode.next;
+                currentNode.next = previousNode;
+                previousNode = currentNode;
+                currentNode = temp;
             }
-            while (counter < count - n)
-            {
-                counter++;
-                currentNode = currentNode.next;
-            }
-
-            var temp = currentNode.next.next;
-            currentNode.next = temp;
-
-            return head;
-        }
-
-        public int getCount(Node node)
-        {
-            Node current = node;
-            int count = 0;
-
-            while (current != null)
-            {
-                count++;
-                current = current.next;
-            }
-
-            return count;
+            return previousNode;
         }
     }
 
