2baca281f264e2ca94cff26afb4886aa54fb232c
nitin <nitinjas.chd@gmail.com>
2022-01-11 13:21:27 +0530

    Solution: Add 2 numbers in linked list

---
+++ LinkedList/Add2Numbers/CodeFile.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class SolutionAddTwoNumbers
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummyNode = new ListNode(0);
+        ListNode list1 = l1, list2 = l2;
+        ListNode currentNode = dummyNode;
+        int carry = 0;
+
+        //Loop on both the lists till null
+        while (list1 != null || list2 != null)
+        {
+            int x = 0;
+            int y = 0;
+            if (list1 != null)
+            {
+                //storing value of node in x
+                x = list1.val;
+            }
+            if (list2 != null)
+            {
+                //storing value of node in y
+                y = list2.val;
+            }
+
+            //Adding individual element of both list one by one. Adding carry so that if there is any
+            //By default carry is 0. update carry with the sum/10, to get carry
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            //Now currenNode.next will point to the sum of 2 numbers without carry and sum is sum%10
+            //e.g 7+3=10, sum%10=0, carry =1
+            currentNode.next = new ListNode(sum % 10);
+            currentNode = currentNode.next;
+
+            if (list1 != null)
+            {
+                list1 = list1.next;
+            }
+            if (list2 != null)
+            {
+                list2 = list2.next;
+            }
+        }
+        //If there is carry while adding last 2 numbers then we can simply point current.next to the carry element
+        if (carry > 0)
+        {
+            currentNode.next = new ListNode(carry);
+        }
+
+        //return dummy.next as dummy initially is pointing to 0
+        return dummyNode.next;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,17 +8,17 @@ namespace Challenges
     class LinkedList
     {
 
-        Node head, head2;
+        ListNode head, head2;
 
-        public class Node
+        public class ListNode
         {
 
-            public int data;
-            public Node next;
+            public int val;
+            public ListNode next;
 
-            public Node(int d)
+            public ListNode(int d)
             {
-                data = d;
+                val = d;
                 next = null;
             }
         }
@@ -29,32 +29,103 @@ namespace Challenges
             LinkedList list2 = new LinkedList();
 
             // creating first linked list
-            list1.head = new Node(-9);
-            list1.head.next = new Node(3);
-           // list1.head.next.next = new Node(4);
+            list1.head = new ListNode(2);
+            list1.head.next = new ListNode(4);
+            list1.head.next.next = new ListNode(3);
 
             // creating first linked list
 
-            list2.head = new Node(5);
-            list2.head.next = new Node(7);
-           // list2.head.next.next = new Node(4);
+            list2.head = new ListNode(5);
+            list2.head.next = new ListNode(6);
+            list2.head.next.next = new ListNode(4);
             //list.head.next.next.next = new Node(1);
             //list.head.next.next.next.next = new Node(5);
             //list.head.next.next.next.next.next = new Node(4);
             //list.head.next.next.next.next.next.next = new Node(6);
-            var sortedList = MergeTwoLists(list1.head, list2.head);
+            var sortedList = AddTwoNumbers(list1.head, list2.head);
         }
 
-        public static Node MergeTwoLists(Node l1, Node l2)
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            var head = new Node(0);
+            ListNode dummyNode = new ListNode(0);
+            ListNode list1 = l1, list2 = l2;
+            ListNode currentNode = dummyNode;
+            int carry = 0;
+
+            //Loop on both the lists till null
+            while (list1 != null || list2 != null)
+            {
+                int x = 0;
+                int y = 0;
+                //check if node in list1 is not null
+                if (list1 != null)
+                {
+                    //storing value of node in x
+                    x = list1.val;
+                }
+                //check if node in list2 is not null
+                if (list2 != null)
+                {
+                    //storing value of node in y
+                    y = list2.val;
+                }
+
+                //Adding individual element of both list one by one. Adding carry so that if there is any
+                //By default carry is 0. update carry with the sum/10, to get carry
+                int sum = x + y + carry;
+                carry = sum / 10;
+
+                //Now currenNode.next will point to the sum of 2 numbers without carry and sum is sum%10
+                //e.g 7+3=10, sum%10=0, carry =1
+                currentNode.next = new ListNode(sum % 10);
+                currentNode = currentNode.next;
+
+         
+                //When addition is done, move list1 and list2 to next position
+                if (list1 != null)
+                {
+                    list1 = list1.next;
+                }
+                if (list2 != null)
+                {
+                    list2 = list2.next;
+                }
+            }
+            //If there is carry while adding last 2 numbers then we can simply point current.next to the carry element
+            if (carry > 0)
+            {
+                currentNode.next = new ListNode(carry);
+            }
+
+            //return dummy.next as dummy initially is pointing to 0
+            return dummyNode.next;
+        }
+
+        private static ListNode ReverseList(ListNode head)
+        {
+            ListNode previousNode = null;
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                var temp = currentNode.next;
+                currentNode.next = previousNode;
+                previousNode = currentNode;
+                currentNode = temp;
+            }
+            return previousNode;
+        }
+
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var head = new ListNode(0);
             var dummyNode = head;
             var list1 = l1;
             var list2 = l2;
 
             while (list1 != null && list2 != null)
             {
-                if (list1.data < list2.data)
+                if (list1.val < list2.val)
                 {
                     dummyNode.next = list1;
                     dummyNode = dummyNode.next;
