bd5188ffa6083b9c5bb8de7ca867be5382782ed3
nitin <nitinjas.chd@gmail.com>
2022-01-31 22:12:24 +0530

    Solution: Daily temperatures

--- Program.cs
+++ Program.cs
@@ -12,69 +12,30 @@ namespace Challenges
 
         public static void Main(String[] args)
         {
-            string s = "{([])}";
-            var isValid = IsValid(s);
+            int[] temp = { 73, 74, 75, 71, 69, 72, 76, 73 };
+            var isValid = DailyTemperatures(temp);
         }
 
-        public static bool IsValid(string s)
+        public static int[] DailyTemperatures(int[] temperatures)
         {
-            Stack<char> stack = new Stack<char>();
+            var stack = new Stack<int>();
+            var result = new int[temperatures.Length];
 
-            char[] str = s.ToCharArray();
-            for (int i = 0; i < str.Length; i++)
+            for (int i = 0; i < temperatures.Length; i++)
             {
-                //this is checking if it contains opening bracket
-                if (isOpening(str[i]))
+                while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
                 {
-                    stack.Push(str[i]);
+                    var index = stack.Pop();
+                    result[index] = i - index;
                 }
 
-                else
-                {
-                    if (stack.Count == 0)
-                    {
-                        return false;
-                    }
-                    //if 
-                    else if (!isMatching(stack.Peek(), str[i]))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        //if elements are matching then remove from stack
-                        stack.Pop();
-                    }
-                }
+                stack.Push(i);
             }
-            return stack.Count == 0 ? true : false;
-        }
-        //method to check if brackets are opening
-        private static bool isOpening(char c)
-        {
-            if (c == '(' || c == '{' || c == '[')
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+
+            return result;
+
         }
 
-        //a: from Stack.b: new value from string
-        private static bool isMatching(char a, char b)
-        {
-            //check opening and closing pair for the bracket
-            if (a == '(' && b == ')' || a == '{' && b == '}' || a == '[' && b == ']')
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
 
---
+++ StacksAndQueue/DailyTemperatures/CodeFile.cs
@@ -0,0 +1,21 @@
+﻿public class SolutionDailyTemperatures
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var stack = new Stack<int>();
+        var result = new int[temperatures.Length];
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+            {
+                var index = stack.Pop();
+                result[index] = i - index;
+            }
+
+            stack.Push(i);
+        }
+
+        return result;
+    }
+}
