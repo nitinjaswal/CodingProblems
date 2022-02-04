15bbf334414f29e34fda426be83c05a8d0b2cb69
nitin <nitinjas.chd@gmail.com>
2022-02-04 18:20:45 +0530

    Solution:Decode string using stacks

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Challenges.Queue.ShortestDistance;
 
 namespace Challenges
@@ -9,52 +10,57 @@ namespace Challenges
     class MyStackUsingSingleQueue
     {
 
-        private Queue<int> q1 = new Queue<int>();
+
+
         public static void Main(String[] args)
         {
+            string s = "3[a]2[bc]";
+            string res = DecodeString(s);
 
         }
 
-        public MyStackUsingSingleQueue()
+        public static string DecodeString(string s)
         {
+            Stack<int> numStack = new Stack<int>();
+            Stack<StringBuilder> strStack = new Stack<StringBuilder>();
 
-        }
+            StringBuilder str = new StringBuilder();
+            int num = 0;
+            char[] c = s.ToCharArray();
 
-        //Push element to the queue
-        public void Push(int x)
-        {
-            //copy all elements from queue1 to queue2
-            while (q1.Count > 0)
+            for (int i = 0; i < c.Length; i++)
             {
-                q2.Enqueue(q1.Dequeue());
+                if (Char.IsDigit(c[i]))
+                {
+                    num = num * 10 + c[i] - '0';
+                }
+                else if (c[i] == '[')
+                {
+                    strStack.Push(str);  //Store str(stringbuilder) in stack
+                    str = new StringBuilder();//Reset str
+                    numStack.Push(num);//push num into the numstack
+                    num = 0;//Reset num
+
+                }
+                else if (c[i] == ']')
+                {
+                    StringBuilder temp = str;
+                    str = strStack.Pop();
+                    int count = numStack.Pop();
+                    while (count > 0)
+                    {
+                        str.Append(temp);
+                        count--;
+                    }
+                }
+                else
+                {
+                    str.Append(c[i]);
+                }
             }
-
-            q1.Enqueue(x);
-
-            //copy back all the elements to queue1
-            while (q2.Count > 0)
-            {
-                q1.Enqueue(q2.Dequeue());
-            }
+            return str.ToString();
         }
 
-        //Dequeue element from queue
-        public int Pop()
-        {
-            return q1.Dequeue();        
-        }
-
-        public int Top()
-        {
-            return q1.Peek();
-        }
-        public bool Empty()
-        {
-            if (q1.Count == 0)
-                return true;
-            else
-                return false;
-        }
     }
 }
 
---
+++ StacksAndQueue/DecodeString/CodeFile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+public class Solution
+{
+    public string DecodeString(string s)
+    {
+        Stack<int> numStack = new Stack<int>();
+        Stack<StringBuilder> strStack = new Stack<StringBuilder>();
+
+        StringBuilder str = new StringBuilder();
+        int num = 0;
+        char[] c = s.ToCharArray();
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            if (Char.IsDigit(c[i]))
+            {
+                num = num * 10 + c[i] - '0';
+            }
+            else if (c[i] == '[')
+            {
+                strStack.Push(str);  //Store str(stringbuilder) in stack
+                str = new StringBuilder();//Reset str
+                numStack.Push(num);//push num into the numstack
+                num = 0;//Reset num
+
+            }
+            else if (c[i] == ']')
+            {
+                StringBuilder temp = str;
+                str = strStack.Pop();
+                int count = numStack.Pop();
+                while (count > 0)
+                {
+                    str.Append(temp);
+                    count--;
+                }
+            }
+            else
+            {
+                str.Append(c[i]);
+            }
+        }
+        return str.ToString();
+    }
+}
