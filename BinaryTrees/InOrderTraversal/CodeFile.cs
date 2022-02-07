6850aab6f68fb936ad72507a6f8a59ea1cd420d9
nitin <nitinjas.chd@gmail.com>
2022-02-07 17:53:54 +0530

    Solution: Binary tree Iterative In-Order traversal

---
+++ BinaryTrees/InOrderTraversal/CodeFile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public class IterativeInorderTraversal
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<int> inOrder = new List<int>();
+        if (root == null) return inOrder;
+        //   stack.Push(root);//Insert root node in stack
+        var node = root;
+        while (node != null || stack.Count > 0)
+        {
+            //iterate each left node and put it into the stack
+            while (node != null)
+            {
+                stack.Push(node);
+                node = node.left;
+            }
+
+            node = stack.Pop();
+            inOrder.Add(node.val);
+            node = node.right;
+        }
+        return inOrder;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,20 +23,20 @@ namespace Challenges
         public Node root;
         public static void Main(String[] args)
         {
-            char[][] input = new char[4][];
-            input[0] = new char[5] { '1', '1', '0', '0', '0' };
-            input[1] = new char[5] { '1', '1', '0', '0', '0' };
-            input[2] = new char[5] { '0', '0', '1', '0', '0' };
-            input[3] = new char[5] { '0', '0', '0', '1', '1' };
+            //char[][] input = new char[4][];
+            //input[0] = new char[5] { '1', '1', '0', '0', '0' };
+            //input[1] = new char[5] { '1', '1', '0', '0', '0' };
+            //input[2] = new char[5] { '0', '0', '1', '0', '0' };
+            //input[3] = new char[5] { '0', '0', '0', '1', '1' };
 
             PreOrderBinaryTree tree = new PreOrderBinaryTree();
             tree.root = new Node(1);
             tree.root.left = new Node(2);
-            tree.root.right = new Node(7);
-            tree.root.left.left = new Node(3);
-            tree.root.left.right = new Node(4);
-            tree.root.left.right.left = new Node(5);
-            tree.root.left.right.right = new Node(6);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+            tree.root.left.right.left = new Node(6);
+            tree.root.left.right.right = new Node(7);
             //tree.root.right.left = new Node(7);
             var res = PreorderTraversal(tree.root);
         }
@@ -44,27 +44,24 @@ namespace Challenges
         public static IList<int> PreorderTraversal(Node root)
         {
             Stack<Node> stack = new Stack<Node>();
-            List<int> preOrder = new List<int>();
-            if (root == null) return preOrder;
-            stack.Push(root);//Insert root node in stack
-            while (stack.Count > 0)
+            List<int> inOrder = new List<int>();
+            if (root == null) return inOrder;
+            //   stack.Push(root);//Insert root node in stack
+            var node = root;
+            while (node!=null || stack.Count>0)
             {
-                var node = stack.Pop();
-
-                //Pop the node from the stack and print it
-                preOrder.Add(node.data);
-
-                //First we push right node as we want to process left node first(LIFO)
-                if (node.right != null)
+                //iterate each left node and put it into the stack
+                while (node != null)
                 {
-                    stack.Push(node.right);
+                    stack.Push(node);
+                    node = node.left;
                 }
-                if (node.left != null)
-                {
-                    stack.Push(node.left);
-                }
+
+                node = stack.Pop();
+                inOrder.Add(node.data);
+                node = node.right;
             }
-            return preOrder;
+            return inOrder;
         }
     }
 
