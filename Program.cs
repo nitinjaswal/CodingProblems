c99287ac4459b981ecb32ee88ea1d9e29628c1b7
nitin <nitinjas.chd@gmail.com>
2022-03-14 22:07:22 +0530

    Recursion: Reverse integer

--- Program.cs
+++ Program.cs
@@ -24,16 +24,23 @@ namespace Challenges
         public static void Main(String[] args)
         {
             int number = 1342;
-            Console.WriteLine(SumOfDigits(number));
+            Console.WriteLine(ReverseNumber(number));
         }
 
-        static int SumOfDigits(int num)
+        static long ReverseNumber(int num)
         {
-            if (num == 0)
+            int digits = (int)Math.Floor(Math.Log10(num) + 1);
+            return helper(num, digits);
+        }
+
+        private static long helper(int n, int digits)
+        {
+            if (n % 10 == 0)
             {
-                return 1;
+                return n;
             }
-            return SumOfDigits(num / 10) * (num % 10);
+            int rem = n % 10;
+            return rem * (long)Math.Pow(10, digits - 1) + helper(n / 10, digits - 1);
         }
 
     }
---
+++ Recursion/ReverseNumber/CodeFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class RecursiveReverseNumber
+{
+    static long ReverseNumber(int num)
+    {
+        int digits = (int)Math.Floor(Math.Log10(num) + 1);
+        return helper(num, digits);
+    }
+
+    private static long helper(int n, int digits)
+    {
+        if (n % 10 == 0)
+        {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (long)Math.Pow(10, digits - 1) + helper(n / 10, digits - 1);
+    }
+}
