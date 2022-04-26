395974609b80ea44b7d86b3e0cfb999e408bb74a
nitin <nitinjas.chd@gmail.com>
2022-04-26 22:35:16 +0530

    Solution: Unique ways to climb stair case

--- Program.cs
+++ Program.cs
@@ -17,48 +17,30 @@ namespace Challenges
     }
     class LinkedList
     {
-        static List<List<int>> output = new List<List<int>>();
+        static Dictionary<int, int> map = new Dictionary<int, int>();
         public static void Main(String[] args)
         {
             int[] nums1 = { 1, 2, 2, 1 };
             int[] nums2 = { 2, 2 };
-            int index = FirstUniqChar("loveleetcode");
+            // int index = FirstUniqChar("loveleetcode");
+            int result = ClimbStairs(6);
 
         }
 
-        public static int FirstUniqChar(string s)
+        public static int ClimbStairs(int n)
         {
-            var charArr = s.ToCharArray();
-            Dictionary<char, int> set = new Dictionary<char, int>();
+            if (map.ContainsKey(n))
+                return map[n];
 
-            //create hashtable and maintain count of each character
-            for (int i = 0; i <= charArr.Length - 1; i++)
+            if (n == 0 || n == 1)
             {
-                if (!set.ContainsKey(charArr[i]))
-                {
-                    set.Add(charArr[i], 1);
-                }
-                else
-                {
-                    //if element exist more than once, then update count
-                    int old = (int)set[charArr[i]];
-                    set[charArr[i]] = old + 1;
-                }
+                return 1;
             }
+            int sum = ClimbStairs(n - 1) + ClimbStairs(n - 2);
 
-           
-            for (int i = 0; i <= charArr.Length - 1; i++)
-            {
-                if (set[charArr[i]].Equals(1))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-
+            map[n] = sum;
+            return sum;
         }
-
     }
 
 }
---
+++ Recursion/ClimbingStairs/CodeFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public class SolutionClimbStairsRecursive
+{
+    static Dictionary<int, int> map = new Dictionary<int, int>();
+    public int ClimbStairs(int n)
+    {
+        if (map.ContainsKey(n))
+            return map[n];
+
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        int sum = ClimbStairs(n - 1) + ClimbStairs(n - 2);
+
+        map[n] = sum;
+        return sum;
+    }
+}
