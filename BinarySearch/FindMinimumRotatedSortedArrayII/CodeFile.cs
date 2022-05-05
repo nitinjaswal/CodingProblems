ca942f5db4b2ba025b487b2167ec6d98bef1d89a
nitin <nitinjas.chd@gmail.com>
2022-05-05 22:30:45 +0530

    Solution: Find Minimum in Rotated Sorted Array II

---
+++ BinarySearch/FindMinimumRotatedSortedArrayII/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿public class SolutionFindMinII
+{
+    public int FindMin(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+
+        int low = 0;
+        int high = nums.Length - 1;
+
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+
+            if (nums[mid] > nums[high])
+            {
+                low = mid + 1;
+            }
+            else if (nums[mid] < nums[high]) //
+            {
+                high = mid;
+            }
+            else
+            {
+                high = high - 1;
+            }
+        }
+        return nums[low];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,46 +20,39 @@ namespace Challenges
     {
         public static void Main(String[] args)
         {
-            char[] letters = { 'c', 'f', 'j' };
-            char smallestCharacter = NextGreatestLetter(letters, 'a');
-        }
-        public static char NextGreatestLetter1(char[] letters, char target)
-        {
-            int i;
-            for (i = 0; i <= letters.Length - 1; i++)
-            {
-                if (letters[i] > target)
-                {
-                    return letters[i];
-                }
-            }
-            return letters[0];
+            int[] nums = { 1, 3, 3 };
+            int smallestCharacter = FindMin(nums);
         }
 
-        public static char NextGreatestLetter(char[] letters, char target)
+        public static int FindMin(int[] nums)
         {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
             int low = 0;
-            int high = letters.Length-1;
-            while (low <= high)
+            int high = nums.Length - 1;
+
+            while (low < high)
             {
                 int mid = low + (high - low) / 2;
-                if (letters[mid] <= target)
+
+
+                if (nums[mid] > nums[high])
                 {
                     low = mid + 1;
                 }
+                else if (nums[mid] < nums[high]) //
+                {
+                    high = mid;
+                }
                 else
                 {
-                    high = mid-1;
+                    high = high - 1;
                 }
             }
-            if (low == letters.Length)
-            {
-                return letters[0];
-            }
-            else
-            {
-                return letters[low];
-            }
+            return nums[low];
         }
     }
 }
