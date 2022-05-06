f7034982994076961ea21bb9e33a52ba1de846ce
nitin <nitinjas.chd@gmail.com>
2022-05-06 21:31:50 +0530

    Solution: Intersection of 2 arrays

---
+++ Arrays/IntersectionTwoArraysII/CodeFile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+public class SolutionIntersectII
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var item in nums1)
+        {
+            if (!dict.ContainsKey(item))
+            {
+                dict.Add(item, 0);
+            }
+
+            dict[item] = 1;
+        }
+
+        var result = new List<int>();
+        foreach (var item in nums2)
+        {
+            if (dict.ContainsKey(item) && dict[item] > 0)
+            {
+                result.Add(item);
+                dict[item] = -1;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -24,36 +24,6 @@ namespace Challenges
             int smallestCharacter = FindMin(nums);
         }
 
-        public static int FindMin(int[] nums)
-        {
-            if (nums.Length == 1)
-            {
-                return nums[0];
-            }
-
-            int low = 0;
-            int high = nums.Length - 1;
-
-            while (low < high)
-            {
-                int mid = low + (high - low) / 2;
-
-
-                if (nums[mid] > nums[high])
-                {
-                    low = mid + 1;
-                }
-                else if (nums[mid] < nums[high]) //
-                {
-                    high = mid;
-                }
-                else
-                {
-                    high = high - 1;
-                }
-            }
-            return nums[low];
-        }
     }
 }
 
