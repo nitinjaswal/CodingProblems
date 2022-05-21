aa06d146908d41167cdd2d4767282cbfe9c60093
nitin <nitinjas.chd@gmail.com>
2022-05-21 16:14:54 +0530

    Best time to buy and sell stock

--- Arrays/BestTimeToBuyAndSellStocks/CodeFile.cs
+++ Arrays/BestTimeToBuyAndSellStocks/CodeFile.cs
@@ -19,6 +19,38 @@
         }
         return maxProfit;
     }
+    public int MaxProfit2(int[] prices)
+    {
+
+        int maxProfit = 0;
+        // let use initialize Left and Right pointer to first and second position of array
+        //Here Left is to buy stock and Right is to sell stock
+        int left = 0;
+        int right = 1;
+
+
+        //Then we initialize our max_profit as 0
+
+        //Now we will start our while loop and we will run till our Right pointer less then length of array
+        while (right <= prices.Length - 1)
+        {
+
+            if (prices[left] < prices[right])
+            {
+                int profit = prices[right] - prices[left];
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+            else
+            {
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
     //brute force approach:O(n^2)
     public static int MaxProfit1(int[] prices)
     {
--- Program.cs
+++ Program.cs
@@ -21,8 +21,8 @@ namespace Challenges
         static ListNode listNode = null;
         public static void Main(String[] args)
         {
-            int[] nums = { 3, 6, 1, 0 };
-            int pivot = DominantIndex(nums);
+            int[] nums = { 7, 1, 5, 3, 6, 4 };
+            int pivot = MaxProfit(nums);
             listNode = new ListNode(1);
             listNode.next = new ListNode(2);
             listNode.next.next = new ListNode(3);
@@ -31,6 +31,31 @@ namespace Challenges
             //listNode.next.next.next.next.next = new ListNode(4);
             //listNode.next.next.next.next.next.next = new ListNode(5);
             //var list = ReverseBetween(listNode, 2, 4);
+        }
+        public static  int MaxProfit(int[] prices)
+        {
+
+            int maxProfit = 0;
+            int left = 0;
+            int right = 1;
+            while (right <= prices.Length - 1)
+            {
+                if (prices[left] < prices[right])
+                {
+                    int profit = prices[right] - prices[left];
+                    if(profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+                else
+                {
+                    left= right;
+                }
+                right++;
+            }
+            return maxProfit;
+
         }
         public static int DominantIndex(int[] nums)
         {
@@ -44,28 +69,18 @@ namespace Challenges
                     maxIndex = i;
                 }
             }
-            bool islargest = false;
             for (int i = 0; i <= nums.Length - 1; i++)
             {
                 if (nums[i] != largestNum && nums[i] * 2 > largestNum)
                 {
-                    islargest = false;
                     return -1;
                 }
                 else
                 {
-                    islargest = true;
+                    continue;
                 }
-            }           
-
-            if (islargest)
-            {
-                return maxIndex;
             }
-            else
-            {
-                return -1;
-            }
+            return maxIndex;
 
         }
     }
