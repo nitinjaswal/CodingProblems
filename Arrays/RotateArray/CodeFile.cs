89e3f472d22873cc2ef5878becd8806a0375d244
nitin <nitinjas.chd@gmail.com>
2022-05-24 23:02:19 +0530

    Solution 189: Rotate the array

---
+++ Arrays/RotateArray/CodeFile.cs
@@ -0,0 +1,21 @@
+﻿public class SolutionRotateReverse
+{
+    public void Rotate(int[] nums, int k)
+    {
+        k = k % nums.Length;
+        Reverse(nums, nums.Length - k, nums.Length - 1);
+        Reverse(nums, 0, nums.Length - 1 - k);
+        Reverse(nums, 0, nums.Length - 1);
+    }
+    private static void Reverse(int[] res, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = res[start];
+            res[start] = res[end];
+            res[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
---
+++ Arrays/RotateArray/CodeFile1.cs
@@ -0,0 +1,29 @@
+﻿public class SolutionRotateExtraArray
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int size = nums.Length;
+        int[] result = new int[size + 1];
+        k = k % size;
+        int i = 0;
+        int j = nums.Length - k;
+        while (i < k)
+        {
+            result[i] = nums[j];
+            i++;
+            j++;
+        }
+        int l = 0;
+        while (i <= nums.Length - 1)
+        {
+            result[i] = nums[l];
+            i++;
+            l++;
+        }
+
+        for (int start = 0; start <= nums.Length - 1; start++)
+        {
+            nums[start] = result[start];
+        }
+    }
+}
