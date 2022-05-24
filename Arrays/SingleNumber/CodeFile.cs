23d35f257d4c8c3e9689fb3de985c70c54bab8e3
nitin <nitinjas.chd@gmail.com>
2022-05-24 11:45:12 +0530

    Solution 136 : Single Number using XOR

---
+++ Arrays/SingleNumber/CodeFile.cs
@@ -0,0 +1,12 @@
+﻿public class SolutionSingleNumber
+{
+    public static int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums)
+        {
+            result = result ^ num;
+        }
+        return result;
+    }
+}
