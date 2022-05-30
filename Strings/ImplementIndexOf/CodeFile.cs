e19c43a5c800257dd3e84c97bacdbc4866f86c93
nitin <nitinjas.chd@gmail.com>
2022-05-30 22:43:43 +0530

    Solution 28: Implement strStr()

---
+++ Strings/ImplementIndexOf/CodeFile.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (needle == "")
+        {
+            return 0;
+        }
+        string temp = "";
+        for (int i = 0; i <= haystack.Length; i++)
+        {
+            temp = haystack.Substring(0, i);
+            if (temp.Contains(needle))
+            {
+                return i - needle.Length;
+            }
+        }
+        return -1;
+    }
+}
