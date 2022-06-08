4bdff970b3e6c8a7801d82e889f8d6f71a90019d
nitin <nitinjas.chd@gmail.com>
2022-06-08 23:11:56 +0530

    Solution 448: Find All Numbers Disappeared in an Array using CYCLIC sort

---
+++ Sorting/CyclicSort/FindAllNumbersDisappearedArray/CodeFile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+public class SolutionFindDisappearedNumbersCyclicSort
+{
+    public IList<int> FindDisappearedNumbers(int[] arr)
+    {
+        //USING: CYCLIC SORT
+        var missingNumbers = new List<int>();
+        for (int i = 0; i <= arr.Length - 1;)
+        {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex])
+            {
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (int i = 0; i <= arr.Length - 1; i++)
+        {
+            if (arr[i] != i + 1)
+            {
+                missingNumbers.Add(i + 1);
+            }
+        }
+        return missingNumbers;
+    }
+}
