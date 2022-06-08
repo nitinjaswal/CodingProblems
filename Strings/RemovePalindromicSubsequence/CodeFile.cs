d0187abc217f8c9fed3b0b8d6b7870cdff02a67b
nitin <nitinjas.chd@gmail.com>
2022-06-08 23:08:19 +0530

    Solution 1332: Remove Palindromic Subsequences

---
+++ Strings/RemovePalindromicSubsequence/CodeFile.cs
@@ -0,0 +1,34 @@
+﻿public class SolutionRemovePalindromeSub
+{
+    public int RemovePalindromeSub(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+
+        if (IsPalindrome(s))
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+    bool IsPalindrome(string s)
+    {
+        int left = 0; int right = s.Length - 1;
+
+        while (left <= right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
