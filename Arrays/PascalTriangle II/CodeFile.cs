0458225663f1c361544791289e0bb2cb71dd4fc6
nitin <nitinjas.chd@gmail.com>
2022-07-25 10:34:20 +0530

    Solution 119: Pascal's Triangle II

---
+++ Arrays/PascalTriangle II/CodeFile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class SolutionPascalTriangleGetRow
+{
+    public IList<int> GetRow(int numRows)
+    {
+        var previousList = new List<int>();
+        previousList.Add(1);
+
+        for (int i = 1; i <= numRows; i++)
+        {
+            var currentList = new List<int>();//Constructing new liust for every row
+            int c = 0;
+            foreach (var item in previousList)
+            {
+                currentList.Add(item + c);//First element will always be 1
+                c = item;
+            }
+
+            currentList.Add(1);//Last element will always be 1
+            previousList = currentList;
+        }
+        return previousList;
+    }
+}
