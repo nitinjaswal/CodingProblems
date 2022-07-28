fdff0068e7ed8a4bda4cf5cfc4b1d85c7b220f7b
nitin <nitinjas.chd@gmail.com>
2022-07-28 16:19:27 +0530

    Updated comments for Set Matrix zeroes

--- Arrays/2DArrays/SetMatrixZeroes/CodeFile2.cs
+++ Arrays/2DArrays/SetMatrixZeroes/CodeFile2.cs
@@ -1,4 +1,4 @@
-﻿public class SolutionSetZeroesEasyToUnderStand
+﻿public class SolutionSetZeroes
 {
     public void SetZeroes(int[][] matrix)
     {
@@ -6,32 +6,27 @@
         int row = matrix.Length;//matrix row length
         int col = matrix[0].Length;//matrix column lenght
         bool isCol = false;
-        bool isRow = false;
         int[] rows = new int[] { row };
 
 
         for (int i = 0; i < row; i++)
         {
-            //check except the first row and column
+            if (matrix[i][0] == 0)
+            {
+                isCol = true;
+            }
+
             // If an element is zero, we set the first element of the corresponding row and column to 0
-            for (int j = 0; j < col; j++)
+            for (int j = 1; j < col; j++)
             {
                 if (matrix[i][j] == 0)
                 {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
-                    if (i == 0)
-                    {
-                        isRow = true;
-                    }
-                    if (j == 0)
-                    {
-                        isCol = true;
-                    }
                 }
             }
         }
-        // process except the first row and column
+
         // Iterate over the array once again and using the first row and first column, update the elements.
         for (int i = 1; i < row; i++)
         {
@@ -43,9 +38,9 @@
                 }
             }
         }
-        //handle the first row
+
         // See if the first row needs to be set to zero as well
-        if (isRow)
+        if (matrix[0][0] == 0)
         {
             for (int j = 0; j < col; j++)
             {
@@ -53,7 +48,6 @@
             }
         }
 
-        //handle the first column
         // See if the first column needs to be set to zero as well
         if (isCol)
         {
