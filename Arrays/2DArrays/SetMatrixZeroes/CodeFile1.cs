84e197620279d4822239db6dfcfd48f323993f77
nitin <nitinjas.chd@gmail.com>
2022-07-28 15:56:22 +0530

    Set Matrix Zeroes(Space efficient solution)

---
+++ Arrays/2DArrays/SetMatrixZeroes/CodeFile1.cs
@@ -0,0 +1,60 @@
+﻿public class SolutionSetZeroesWithoutSpace
+{
+    public void SetZeroes(int[][] matrix)
+    {
+
+        int row = matrix.Length;//matrix row length
+        int col = matrix[0].Length;//matrix column lenght
+        bool isCol = false;
+        int[] rows = new int[] { row };
+
+
+        for (int i = 0; i < row; i++)
+        {
+            if (matrix[i][0] == 0)
+            {
+                isCol = true;
+            }
+
+            // If an element is zero, we set the first element of the corresponding row and column to 0
+            for (int j = 1; j < col; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Iterate over the array once again and using the first row and first column, update the elements.
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 1; j < col; j++)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // See if the first column needs to be set to zero as well
+        if (isCol)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -49,8 +49,8 @@ namespace Challenges
 
         static int[][] intMatrix = new int[][]
            {
-                new int[] {1,1,1 },
-                new int[] {1,0,1 },
+                new int[] {0,1,0 },
+                new int[] {1,5,1 },
                 new int[] {1,1,1 },
           };
 
@@ -70,37 +70,55 @@ namespace Challenges
         {
             int row = matrix.Length;//matrix row length
             int col = matrix[0].Length;//matrix column lenght
+            bool isCol = false;
+            int[] rows = new int[] { row };
 
-            HashSet<int> rows = new HashSet<int>();
-            HashSet<int> cols = new HashSet<int>();
 
-            //mark each row and col when it is ZERO and add row and col in Hash
             for (int i = 0; i < row; i++)
             {
-                for (int j = 0; j < col; j++)
+                if (matrix[i][0] == 0)
+                {
+                    isCol = true;
+                }
+
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                for (int j = 1; j < col; j++)
                 {
                     if (matrix[i][j] == 0)
                     {
-                        rows.Add(i);
-                        cols.Add(j);
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
                     }
                 }
             }
 
-            //Now for added row and col in hash we check for [i][j]
-            for (int i = 0; i < row; i++)
+            // Iterate over the array once again and using the first row and first column, update the elements.
+            for (int i = 1; i < row; i++)
+            {
+                for (int j = 1; j < col; j++)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            // See if the first row needs to be set to zero as well
+            if (matrix[0][0] == 0)
             {
                 for (int j = 0; j < col; j++)
                 {
-                    if (rows.Contains(i) || cols.Contains(j))
-                    {
-                        matrix[i][j] = 0;
-                        //eg
-                        //matrix[0][1]=0;
-                        //matrix[1][0]=0
-                        //matrix[1][1]=0
-                        //matrix[1][2]=0
-                    }
+                    matrix[0][j] = 0;
+                }
+            }
+
+            // See if the first column needs to be set to zero as well
+            if (isCol)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    matrix[i][0] = 0;
                 }
             }
         }
