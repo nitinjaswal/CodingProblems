085a9ecbac337a9a9552807000849d33c97e8acf
nitin <nitinjas.chd@gmail.com>
2022-07-29 22:08:17 +0530

    Three consecutive one: optimized

---
+++ Arrays/ThreeConsecutiveOdds/CodeFile1.cs
@@ -0,0 +1,15 @@
+﻿public class SolutionFaster
+{
+    public bool ThreeConsecutiveOdds(int[] arr)
+    {
+        bool isConsecutiveOdd = false; ;
+        for (int i = 0; i < arr.Length - 2; i++)
+        {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0)
+            {
+                isConsecutiveOdd = true;
+            }
+        }
+        return isConsecutiveOdd;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -68,23 +68,15 @@ namespace Challenges
 
         public static bool ThreeConsecutiveOdds(int[] arr)
         {
-            int oddStreak = 0;
-            for (int i = 0; i < arr.Length; i++)
+            bool isConsecutiveOdd = false; ;
+            for (int i = 0; i < arr.Length - 2; i++)
             {
-                if (arr[i] % 2 != 0 || arr[i] == 1)
-                {                    
-                    oddStreak++;
-                    if (oddStreak == 3)
-                    {
-                        return true;
-                    }
-                 }
-                else
+                if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0)
                 {
-                    oddStreak = 0;
+                    isConsecutiveOdd = true;
                 }
             }
-            return false;
+            return isConsecutiveOdd;
         }
 
         public static int NumIslands(char[][] grid)
