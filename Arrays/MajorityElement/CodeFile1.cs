5a6756297b3f69241acf81569acc221d33e92420
nitin <nitinjas.chd@gmail.com>
2022-08-02 08:10:41 +0530

    Solution 169: Majority element using Hashtable

---
+++ Arrays/MajorityElement/CodeFile1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class SolutionMajorityElementHashtable
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> set = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!set.ContainsKey(nums[i]))
+            {
+                set.Add(nums[i], 1);
+            }
+            else
+            {
+                set[nums[i]]++;
+            }
+        }
+        int n = nums.Length / 2;
+        int result = set.FirstOrDefault(x => x.Value > n).Key;
+        return result;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -40,9 +40,9 @@ namespace Challenges
 
         static int[][] arr = new int[][]
            {
-               new int[]{7,34,45,10,12,27,13 },
-               new int[]{27,21,45,10,12,13 },
-               //new int[]{3,4,5,6 },
+               new int[]{1,3,5,7 },
+               new int[]{10,11,16,20 },
+               new int[]{23,30,34,60},
            };
 
 
@@ -50,37 +50,30 @@ namespace Challenges
         {
             //ListNode node = new ListNode(1);
             //node.next = new ListNode(2);
+            int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+            int check = MajorityElement(nums);
 
-            var list = Intersection(arr);
         }
-
-        public static IList<int> Intersection(int[][] nums)
+        public static int MajorityElement(int[] nums)
         {
-            int row = nums.Length;
-            int col = nums[0].Length;
-
             Dictionary<int, int> set = new Dictionary<int, int>();
-
-            for (int i = 0; i < row; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                for (int j = 0; j < nums[i].Length; j++)
+                if (!set.ContainsKey(nums[i]))
                 {
-                    if (!set.ContainsKey(nums[i][j]))
-                    {
-                        set.Add(nums[i][j], 1);
-                    }
-                    else
-                    {
-                        set[nums[i][j]]++;
-                    }
+                    set.Add(nums[i],1);
+                }
+                else
+                {
+                    set[nums[i]]++;
                 }
             }
-            var result = set.Where(x => x.Value == nums.Length).Select(x => x.Key).OrderBy(x => x);
-            return result.ToList();
+            int n = nums.Length / 2;
+            int result = set.FirstOrDefault(x => x.Value > n).Key;
+            return result;
         }
     }
 }
-
 
 
 
