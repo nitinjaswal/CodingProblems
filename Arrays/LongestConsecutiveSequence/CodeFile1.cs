7ef249dd9daf34d32ca2a0d7e8a2ac397c863fc2
nitin <nitinjas.chd@gmail.com>
2022-08-04 23:44:29 +0530

    Solution 128:  Longest Consecutive Sequence in O(n) time and extra space

---
+++ Arrays/LongestConsecutiveSequence/CodeFile1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SolutionLongestConsecutivehashMap
+{
+    public int LongestConsecutive(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            return 0;
+        }
+
+
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (var item in arr)
+        {
+            set.Add(item);
+        }
+
+        int longestStreak = 0;
+        foreach (int num in arr)
+        {
+            if (!set.Contains(num - 1))
+            {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.Contains(currentNum + 1))
+                {
+                    currentStreak++;
+                    currentNum = currentNum + 1;
+                }
+                longestStreak = Math.Max(currentStreak, longestStreak);
+            }
+        }
+        return longestStreak;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -50,12 +50,44 @@ namespace Challenges
         {
             //ListNode node = new ListNode(1);
             //node.next = new ListNode(2);
-            int[] nums = { 3, 2, 1, 5, 6, 4 };
-
+            int[] nums = { 100, 4, 200, 1, 3, 2 };
+            int seq = LongestConsecutive(nums);
 
         }
 
-      
+        public static int LongestConsecutive(int[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+           
+
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (var item in arr)
+            {
+                set.Add(item);
+            }
+
+            int longestStreak = 0;
+            foreach (int num in arr)
+            {
+                if (!set.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int currentStreak = 1;
+
+                    while (set.Contains(currentNum + 1))
+                    {
+                        currentStreak++;
+                        currentNum = currentNum + 1;
+                    }
+                    longestStreak = Math.Max(currentStreak, longestStreak);
+                }
+            }
+            return longestStreak;
+        }
     }
 }
 
