9cb70be4e1d5fbeed40b21f72ba466ce062f73da
nitin <nitinjas.chd@gmail.com>
2022-09-04 00:29:03 +0530

    Concatenation of array

---
+++ Arrays/ConcatenationArray/CodeFile.cs
@@ -0,0 +1,14 @@
+﻿public class SolutionGetConcatenation
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] newArray = new int[nums.Length * 2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            newArray[i] = nums[i];
+            newArray[i + nums.Length] = nums[i];
+        }
+
+        return newArray;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -50,44 +50,24 @@ namespace Challenges
         {
             //ListNode node = new ListNode(1);
             //node.next = new ListNode(2);
-            int[] nums = { 100, 4, 200, 1, 3, 2 };
-            int seq = LongestConsecutive(nums);
+            int[] nums = { 1, 2, 1 };
+            var array = GetConcatenation(nums);
+
 
         }
 
-        public static int LongestConsecutive(int[] arr)
+        public static int[] GetConcatenation(int[] nums)
         {
-            if (arr.Length == 0)
+            int[] newArray = new int[nums.Length * 2];
+            for (int i = 0; i < nums.Length; i++)
             {
-                return 0;
-            }
-           
-
-            HashSet<int> set = new HashSet<int>();
-
-            foreach (var item in arr)
-            {
-                set.Add(item);
+                newArray[i] = nums[i];
+                newArray[i + nums.Length] = nums[i];
             }
 
-            int longestStreak = 0;
-            foreach (int num in arr)
-            {
-                if (!set.Contains(num - 1))
-                {
-                    int currentNum = num;
-                    int currentStreak = 1;
-
-                    while (set.Contains(currentNum + 1))
-                    {
-                        currentStreak++;
-                        currentNum = currentNum + 1;
-                    }
-                    longestStreak = Math.Max(currentStreak, longestStreak);
-                }
-            }
-            return longestStreak;
+            return newArray;
         }
+
     }
 }
 
