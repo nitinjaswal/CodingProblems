5ad336b6e0ad2909abf64d5ef85a57d8ed1be735
nitin <nitinjas.chd@gmail.com>
2022-09-04 00:34:40 +0530

    Solution 1920: Build Array from Permutation

---
+++ Arrays/BuildArray/CodeFile.cs
@@ -0,0 +1,14 @@
+﻿public class SolutionBuildArray
+{
+    public int[] BuildArray(int[] nums)
+    {
+        int[] newArray = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            newArray[i] = nums[nums[i]];
+        }
+
+        return newArray;
+    }
+}
