72ef840bb333635e85f56dc8454b6c11be76cc7d
nitin <nitinjas.chd@gmail.com>
2022-09-20 10:44:27 +0530

    Solution 11: Container with most water

---
+++ Arrays/ContainerWithMostWater/CodeFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class SolutionMaxArea
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+
+        //using 2 pointers approach
+        int left = 0;
+        int right = height.Length - 1;
+
+        while (left < right)
+        {
+            int currArea = Math.Min(height[left], height[right]) * (right - left);//area formula
+            maxArea = Math.Max(maxArea, currArea);
+
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    //Brute force approach: Giving Time limit exceeded error
+    public static int MaxArea1(int[] height)
+    {
+        int maxArea = 0;
+
+        for (int i = 0; i < height.Length; i++)
+        {
+            for (int j = i; j < height.Length; j++)
+            {
+                int currArea = Math.Min(height[j], height[i]) * (j - i);
+                maxArea = Math.Max(maxArea, currArea);
+            }
+        }
+        return maxArea;
+    }
+}
