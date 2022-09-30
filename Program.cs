c18929b72b7016e3dcc8d24281d9ed7cc78bf05a
nitin <nitinjas.chd@gmail.com>
2022-09-30 22:26:58 +0530

    62. Unique Paths

--- Program.cs
+++ Program.cs
@@ -87,8 +87,30 @@ namespace Challenges
             }
             return maxArea;
         }
+        private Dictionary<String, int> map = new Dictionary<String, int>();
+        public int UniquePaths(int m, int n)
+        {
+            if (m == 1 || n == 1)
+            {
+                return 1;
+            }
 
-        
+            // using memoization to cehck if cell value is already calculated
+            string cell = m + "," + n;
+            if (map.ContainsKey(cell))
+            {
+                return map[cell];
+            }
+
+            // Down moves: reducing row
+            int left = UniquePaths(m: m - 1, n);
+            // Right moves: reducing columns
+            int right = UniquePaths(m, n: n - 1);
+            map.Add(cell, left + right);
+
+
+            return left + right;
+        }
         public static int MaxArea(int[] height)
         {
             int maxArea = 0;
