c8bacc57e6e5320eee2de525a97b64545282ebc7
nitin <nitinjas.chd@gmail.com>
2022-10-30 11:39:54 +0530

    560 : Subarray Sum Equals K Brute Force

---
+++ Arrays/SubarraySumEqualsK/CodeFile.cs
@@ -0,0 +1,22 @@
+﻿
+
+public class SolutionSubArraySumBruteForce
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                sum = sum + nums[j];
+                if (sum == k)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
