c8e5ccf9bcac86c8709557b86573619e11a777e0
nitin <nitinjas.chd@gmail.com>
2022-11-01 10:23:18 +0530

    Print paths including diagnol

---
+++ Arrays/2DArrays/UniquePaths/CodeFile2.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+public class SolutionDiagnolPaths
+{
+    public static void PrintDiagnolpaths(string p, int m, int n)
+    {
+
+        if (m == 1 && n == 1)
+        {
+            Console.WriteLine(p);
+            return;
+        }
+        if (m > 1 && n > 1)
+        {
+            PrintDiagnolpaths(p: p + 'D', m: m - 1, n: n - 1);
+        }
+        if (m > 1)
+        {
+            PrintDiagnolpaths(p: p + 'V', m: m - 1, n);
+        }
+        if (n > 1)
+        {
+            PrintDiagnolpaths(p: p + 'H', m, n: n - 1);
+        }
+
+    }
+}
+
