7fbd1116176ccd222503802b50b1eb616f51bbcd
nitin <nitinjas.chd@gmail.com>
2022-11-01 10:13:14 +0530

    Print Unique paths

---
+++ Arrays/2DArrays/UniquePaths/CodeFile1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SolutionPrintpaths
+{
+    private Dictionary<String, int> map = new Dictionary<String, int>();
+    public static void Printpaths(string p, int m, int n)
+    {
+
+        if (m == 1 && n == 1)
+        {
+            Console.WriteLine(p);
+            return;
+        }
+
+        if (m > 1)
+        {
+            Printpaths(p: p + 'D', m: m - 1, n);
+        }
+        if (n > 1)
+        {
+            Printpaths(p: p + 'R', m, n: n - 1);
+        }
+    }
+}
