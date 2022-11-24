84f979771610764bfea66c7c065449c97f46a7cd
NITIN\jaswa <nitinjas.chd@gmail.com>
2022-11-24 22:04:28 +0530

    54. Spiral Matrix 59. Spiral Matrix II

---
+++ Arrays/2DArrays/SpiralMatrix/CodeFile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+public class SolutionSpiralOrder
+{
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        int left = 0; int right = matrix[0].Length;
+        int top = 0; int bottom = matrix.Length;
+        var output = new List<int>();
+        while (left < right && top < bottom)
+        {
+            //Traverse left to right from top row
+            for (int i = left; i < right; i++)
+            {
+                output.Add(matrix[top][i]);
+            }
+            top++;
+
+            //Traversing top to bottom from Right column
+            for (int j = top; j < bottom; j++)
+            {
+                output.Add(matrix[j][right - 1]);
+            }
+            right--;
+
+            //This check is added if the Matrix is (1 x n) or (n x 1)
+            if (!(left < right && top < bottom))
+            {
+                break;
+            }
+
+            //Traversing right to left from Bottom-1 Row
+            for (int i = right - 1; i >= left; i--)
+            {
+                output.Add(matrix[bottom - 1][i]);
+            }
+            bottom--;
+
+            //Traversing bottom to top
+            for (int j = bottom - 1; j >= top; j--)
+            {
+                output.Add(matrix[j][left]);
+            }
+            left++;
+        }
+        return output;
+    }
+}
---
+++ Arrays/2DArrays/SpiralMatrixII/CodeFile.cs
@@ -0,0 +1,53 @@
+﻿public class SolutionGenerateMatrix
+{
+    public int[][] GenerateMatrix(int n)
+    {
+        int[][] matrix = new int[n][];
+
+        for (int i = 0; i < n; i++)
+            matrix[i] = new int[n];
+
+        int rowStart = 0;
+        int rowEnd = n - 1;
+        int colStart = 0;
+        int colEnd = n - 1;
+
+        int num = 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd)
+        {
+
+            //Fill left to right top row
+            for (int i = colStart; i <= colEnd; i++)
+            {
+                matrix[rowStart][i] = num++;
+            }
+            rowStart++;
+
+            //Fill top to bottom last column
+            for (int i = rowStart; i <= rowEnd; i++)
+            {
+                matrix[i][colEnd] = num++;
+            }
+            colEnd--;
+
+
+            //Fill Right to Left
+            for (int i = colEnd; i >= colStart; i--)
+            {
+                if (rowStart <= rowEnd)
+                    matrix[rowEnd][i] = num++;
+            }
+            rowEnd--;
+
+            //Fill bottom to top
+            for (int i = rowEnd; i >= rowStart; i--)
+            {
+                if (colStart <= colEnd)
+                    matrix[i][colStart] = num++;
+            }
+            colStart++;
+        }
+        return matrix;
+    }
+}
