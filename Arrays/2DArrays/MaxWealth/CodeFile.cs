7b405ab5120890bfcd5af439465bd6adb18b3df0
NITIN\jaswa <nitinjas.chd@gmail.com>
2022-12-07 21:54:05 +0530

    1672. Richest Customer Wealth

---
+++ Arrays/2DArrays/MaxWealth/CodeFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class SolutionMaximumWealth
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int row = accounts.Length;
+        int col = accounts[0].Length - 1;
+
+        int maxWealth = 0;
+        for (int i = 0; i < row; i++)
+        {
+            int currentSum = 0;
+            for (int j = 0; j <= col; j++)
+            {
+                currentSum = currentSum + accounts[i][j];
+            }
+            maxWealth = Math.Max(currentSum, maxWealth);
+
+        }
+        return maxWealth;
+    }
+}
