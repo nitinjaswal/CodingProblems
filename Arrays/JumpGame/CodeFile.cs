cb06e7155be8de923a8ea9e3aae2ecc12b588ef5
NITIN\jaswa <nitinjas.chd@gmail.com>
2022-12-07 10:46:20 +0530

    55. Jump Game

---
+++ Arrays/JumpGame/CodeFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class SolutionCanJump
+{
+    public bool CanJump(int[] nums)
+    {
+        int reach = 0;
+        for (int i = 0; i <= reach; i++)
+        {
+            reach = Math.Max(reach, i + nums[i]);
+            if (reach >= nums.Length - 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
