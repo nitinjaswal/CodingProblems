05796dfb9d30ec1eb2959fb3ffcacb23a464b5e8
NITIN\jaswa <nitinjas.chd@gmail.com>
2022-12-07 23:12:33 +0530

    1470. Shuffle the Array

---
+++ Arrays/ShuffleArray/CodeFile.cs
@@ -0,0 +1,20 @@
+﻿public class SolutionShuffle
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        int[] result = new int[nums.Length];
+        int j = 0;
+        for (int i = 0; i < nums.Length;)
+        {
+            if (i % 2 == 0)
+            {
+                result[i++] = nums[j++];
+            }
+            else
+            {
+                result[i++] = nums[n++];
+            }
+        }
+        return result;
+    }
+}
