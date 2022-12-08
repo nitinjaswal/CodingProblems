9ba1f01b7edf7ccc5493c1b999be2d1ef4959a5a
NITIN\jaswa <nitinjas.chd@gmail.com>
2022-12-08 22:32:37 +0530

    1431. Kids With the Greatest Number of Candies

---
+++ Arrays/KidsWithGreatestCandies/CodeFile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class SolutionKidsWithCandies
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var result = new List<bool>();
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        int max = 0;
+        for (int i = 0; i < candies.Length; i++)
+        {
+            max = Math.Max(max, candies[i]);
+        }
+
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (candies[i] + extraCandies >= max)
+            {
+                result.Add(true);
+            }
+            else
+            {
+                result.Add(false);
+            }
+        }
+        return result;
+    }
+}
