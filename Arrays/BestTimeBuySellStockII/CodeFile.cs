9fffe3eb525dd00ec7646699c40cc0776cd42acb
NITIN\jaswa <nitinjas.chd@gmail.com>
2022-12-08 23:27:22 +0530

    122. Best Time to Buy and Sell Stock II

---
+++ Arrays/BestTimeBuySellStockII/CodeFile.cs
@@ -0,0 +1,16 @@
+﻿public class SolutionMaxProfit
+{
+    public int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            if (prices[i] < prices[i + 1])
+            {
+                maxProfit = maxProfit + (prices[i + 1] - prices[i]);
+            }
+        }
+        return maxProfit;
+    }
+}
