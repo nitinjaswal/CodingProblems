060244b7445f7ad0014bc8878c4dcb1fcdff1c71
NITIN\jaswa <nitinjas.chd@gmail.com>
2023-01-01 21:26:34 +0530

    290. Word Pattern

---
+++ Strings/WordPattern/CodeFile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+public class SolutionWordPattern
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        Dictionary<char, string> dic = new Dictionary<char, string>();
+        string[] words = s.Split(" ");
+
+        //Check length of words for mapping in both string
+        if (pattern.Length != words.Length)
+        {
+            return false;
+        }
+
+        for (int index = 0; index < words.Length; index++)
+        {
+            if (!dic.ContainsKey(pattern[index]))
+            {
+                //it means for different key, same value is present
+                if (dic.ContainsValue(words[index]))
+                {
+                    return false;
+                }
+                dic.Add(pattern[index], words[index]);
+            }
+            else if (dic[pattern[index]] != words[index])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
